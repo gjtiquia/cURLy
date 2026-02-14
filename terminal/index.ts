40611bc8a9bb67e302da846edd69a93103ecdc4b
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-13 22:01:56 -0500

    basic terminal rendering

--- terminal/index.ts
+++ terminal/index.ts
@@ -1,1 +1,48 @@
-console.log("Hello via Bun!");
+const isANSISupported = true;
+// const isANSISupported = false; // ANSI support is not guaranteed in Windows
+
+const RESET = "\x1b[0m";
+const HIDE_CURSOR = "\x1b[?25l";
+const SHOW_CURSOR = "\x1b[?25h";
+const CURSOR_HOME = "\x1b[H";
+const CLEAR = "\x1b[2J";
+
+process.on("exit", cleanup); // Regular exit
+process.on("SIGINT", cleanupAndExit); // Ctrl-C, does not exit by default, need to manually exit
+process.on("SIGTERM", cleanupAndExit); // Terminated by terminal
+
+for (let i = 0; i < 5; i++) {
+    const bufferArray = "     ".split("");
+    bufferArray[i] = "x";
+
+    const buffer = bufferArray.join("");
+
+    clearAndDrawBuffer(buffer + "\n" + buffer);
+    await Bun.sleep(500);
+}
+
+function clearAndDrawBuffer(buffer: string) {
+    if (isANSISupported) {
+        // hides the cursor and does a single write, preventing flicker
+        process.stdout.write(CLEAR + HIDE_CURSOR + CURSOR_HOME + buffer + RESET);
+    }
+    else {
+        console.clear();
+        console.log(buffer)
+    }
+}
+
+function cleanupAndExit() {
+    cleanup();
+    process.exit(0);
+}
+
+function cleanup() {
+    if (isANSISupported) {
+        process.stdout.write(CLEAR + CURSOR_HOME + SHOW_CURSOR);
+    }
+    else {
+        console.clear();
+    }
+}
+

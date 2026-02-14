1504ef7ced887a3da71f2728f496054d921c2767
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-14 00:06:34 -0500

    snake movement wip

--- terminal/src/index.ts
+++ terminal/src/index.ts
@@ -1,17 +1,74 @@
 import * as ANSI from "./ANSI";
 
+// TODO : for rendering on web, <pre> seems faster than canvas
+
 process.on("exit", cleanup); // Regular exit
 process.on("SIGINT", cleanupAndExit); // Ctrl-C, does not exit by default, need to manually exit
 process.on("SIGTERM", cleanupAndExit); // Terminated by terminal
 
-for (let i = 0; i < 5; i++) {
-    const bufferArray = "     ".split("");
-    bufferArray[i] = "x";
+const FPS = 10;
+const DELTA_TIME_MS = 1000 / FPS
 
-    const buffer = bufferArray.join("");
+const WIDTH = 40;
+const HEIGHT = 10;
 
-    clearAndDrawBuffer(buffer + "\n" + buffer);
-    await Bun.sleep(500);
+const BG_CHAR = " ";
+const SNAKE_CHAR = "x";
+
+// init game logic
+const snakeHeadPos = [0, HEIGHT / 2]; // x,y
+
+// init draw
+const canvas = createCanvas();
+
+while (true) {
+
+    // game logic
+    snakeHeadPos[0] = (snakeHeadPos[0]! + 1) % WIDTH
+
+    // draw
+    resetCanvas(canvas);
+    drawChar(canvas, snakeHeadPos[0], snakeHeadPos[1]!, SNAKE_CHAR);
+
+    // render
+    const buffer = canvasToStringBuffer(canvas);
+    clearAndDrawBuffer(buffer);
+
+    // frame
+    // TODO : can see multiplayer book on their suggested architecture
+    await Bun.sleep(DELTA_TIME_MS);
+}
+
+function createCanvas() {
+    const canvas: string[][] = [];
+    for (let y = 0; y < HEIGHT; y++) {
+        const row: string[] = [];
+        for (let x = 0; x < WIDTH; x++) {
+            row.push(BG_CHAR)
+        }
+        canvas.push(row);
+    }
+    return canvas;
+}
+
+function resetCanvas(canvas: string[][]) {
+    for (let y = 0; y < canvas.length; y++) {
+        for (let x = 0; x < canvas[y]!.length; x++) {
+            canvas[y]![x] = BG_CHAR
+        }
+    }
+    return canvas;
+}
+
+function drawChar(canvas: string[][], x: number, y: number, char: string) {
+    canvas[y]![x] = char;
+}
+
+function canvasToStringBuffer(canvas: string[][]) {
+    let buffer = "";
+    for (let row of canvas)
+        buffer += row.join("") + "\n";
+    return buffer
 }
 
 function clearAndDrawBuffer(buffer: string) {

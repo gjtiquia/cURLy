6d3743bbf93560b540b605782742e8cb8c4b8acc
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-14 11:59:41 -0500

    listen to SIGINT and SIGTERM

--- terminal-go/main.go
+++ terminal-go/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -10,14 +13,33 @@ const FPS = 10
 const DELTA_TIME_MS = 1000 / FPS
 
 func main() {
+	defer cleanup() // called at the end if no SIGINT or SIGTERM is received
+	go listenToSIGINTAndSIGTERM(cleanup)
 
 	// game loop
-	for i := 0; i < 10; i++ {
+	for { // "'while' is spelled 'for' in Go"
 		clearAndDrawBuffer("hello world\n")
 		time.Sleep(DELTA_TIME_MS * time.Millisecond)
 	}
+}
 
-	cleanup()
+func listenToSIGINTAndSIGTERM(cleanupFunc func()) {
+	// create a channel, type os.Signal, buffer 1 (required by signal.Notify)
+	channel := make(chan os.Signal, 1)
+
+	// notify channel on os.Interrupt (SIGINT) or SIGTERM
+	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
+
+	// blocked until receives a notification from channel
+	<-channel
+	// receivedSignal := <-channel // go does not allow unused variables
+	// fmt.Println("receivedSignal", receivedSignal)
+
+	// need to manually cleanup cuz deferred calls will not be called after exit
+	cleanupFunc()
+
+	// caught the signal myself, so also need to exit myself, as it overrides the default behavior
+	os.Exit(0)
 }
 
 // ANSI
@@ -29,11 +51,21 @@ const CURSOR_HOME = "\x1b[H"
 const CLEAR = "\x1b[2J"
 
 func clearAndDrawBuffer(buffer string) {
-	fmt.Print(CLEAR + HIDE_CURSOR + CURSOR_HOME + buffer + RESET)
+	const hideCursor = true
+	// const hideCursor = false
+
+	if hideCursor {
+		fmt.Print(CLEAR + HIDE_CURSOR + CURSOR_HOME + buffer + RESET)
+	} else {
+		fmt.Print(CLEAR + CURSOR_HOME + buffer + RESET)
+	}
 }
 
 func cleanup() {
+	// fmt.Println("cleanup")
+
 	const clear = true
+	// const clear = false
 
 	if clear {
 		fmt.Print(CLEAR + CURSOR_HOME + SHOW_CURSOR)

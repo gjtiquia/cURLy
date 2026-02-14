bb5a1a04ddcff592eb693d49ab506f645ac5982f
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-14 14:23:49 -0500

    refactor ansi package

--- terminal-go/main.go
+++ terminal-go/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gjtiquia/cURLy/terminal-go/ansi"
 	"os"
 	"os/signal"
 	"syscall"
@@ -42,34 +42,10 @@ func listenToSIGINTAndSIGTERM(cleanupFunc func()) {
 	os.Exit(0)
 }
 
-// ANSI
-// TODO : test for CMD and PowerShell support, eg. forcing ANSI support in Windows
-const RESET = "\x1b[0m"
-const HIDE_CURSOR = "\x1b[?25l"
-const SHOW_CURSOR = "\x1b[?25h"
-const CURSOR_HOME = "\x1b[H"
-const CLEAR = "\x1b[2J"
-
 func clearAndDrawBuffer(buffer string) {
-	const hideCursor = true
-	// const hideCursor = false
-
-	if hideCursor {
-		fmt.Print(CLEAR + HIDE_CURSOR + CURSOR_HOME + buffer + RESET)
-	} else {
-		fmt.Print(CLEAR + CURSOR_HOME + buffer + RESET)
-	}
+	ansi.ClearAndDrawBuffer(buffer)
 }
 
 func cleanup() {
-	// fmt.Println("cleanup")
-
-	const clear = true
-	// const clear = false
-
-	if clear {
-		fmt.Print(CLEAR + CURSOR_HOME + SHOW_CURSOR)
-	} else {
-		fmt.Print(SHOW_CURSOR)
-	}
+	ansi.Cleanup()
 }

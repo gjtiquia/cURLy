b3709a80d24cb016e2e07d7c8e29f3a7edefe218
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-15 23:56:22 -0500

    error stacktrace and log to file on panic

--- terminal-go/v2/main.go
+++ terminal-go/v2/main.go
@@ -6,15 +6,30 @@ import (
 
 	"github.com/gdamore/tcell/v3"
 	"github.com/gdamore/tcell/v3/color"
+	"github.com/pkg/errors"
 )
 
 func main() {
+	// logging setup
+	file, err := InitLogFile("log.txt")
+	if err != nil {
+		log.Panicf("%+v", errors.WithStack(err))
+	}
+	defer file.Close()
+	defer LogPanicBeforeFileClose()
+
+	// TODO : remove
+	// panic(errors.WithStack(errors.New("hi i am panicking")))
+
+	// tcell setup
 	s, err := tcell.NewScreen()
 	if err != nil {
-		log.Fatalf("%+v", err)
+		log.Printf("%+v", err)
+		return
 	}
 	if err := s.Init(); err != nil {
-		log.Fatalf("%+v", err)
+		log.Printf("%+v", err)
+		return
 	}
 
 	// Set default text style
@@ -27,6 +42,8 @@ func main() {
 	s.Put(0, 0, "H", defStyle)
 	s.Put(1, 0, "i", defStyle)
 	s.Put(2, 0, "!", defStyle)
+
+	s.PutStr(0, 1, "Hello World!")
 
 	quit := func() {
 		s.Fini()
@@ -48,5 +65,28 @@ func main() {
 				quit()
 			}
 		}
+	}
+}
+
+func InitLogFile(filename string) (*os.File, error) {
+	// truncate means delete contents on open, create if doesnt exist, write-only
+	const fileFlags = os.O_TRUNC | os.O_CREATE | os.O_WRONLY
+
+	// read = 4, write = 2, execute = 1; 6 = 4 + 2 (read write); 0 = octal; 666 = owner/group/others
+	const filePerm = 0666
+
+	file, err := os.OpenFile(filename, fileFlags, filePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	log.SetOutput(file)
+	return file, nil
+}
+
+func LogPanicBeforeFileClose() {
+	if r := recover(); r != nil {
+		log.Println("logging panic before file close")
+		log.Panicf("%+v", r)
 	}
 }

57b0ca43e52f06a5cbaf32df964bfea3d7c023d8
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-15 23:18:51 -0500

    tcell hello world

--- terminal-go/v2/main.go
+++ terminal-go/v2/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/gdamore/tcell/v3"
+	"github.com/gdamore/tcell/v3/color"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	s, err := tcell.NewScreen()
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+	if err := s.Init(); err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	// Set default text style
+	defStyle := tcell.StyleDefault.Background(color.Reset).Foreground(color.Reset)
+	s.SetStyle(defStyle)
+
+	// Clear screen
+	s.Clear()
+
+	s.Put(0, 0, "H", defStyle)
+	s.Put(1, 0, "i", defStyle)
+	s.Put(2, 0, "!", defStyle)
+
+	quit := func() {
+		s.Fini()
+		os.Exit(0)
+	}
+	for {
+		// Update screen
+		s.Show()
+
+		// Poll event (can be used in select statement as well)
+		ev := <-s.EventQ()
+
+		// Process event
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			s.Sync()
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+				quit()
+			}
+		}
+	}
 }

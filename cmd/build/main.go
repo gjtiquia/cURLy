79920aee540d2841df91fe806d1324bbd640846c
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-18 16:15:43 -0500

    wip fix build script

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -17,7 +18,8 @@ func main() {
 	listCmd := exec.Command("go", "tool", "dist", "list")
 	bytes, err := listCmd.Output()
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "go tool dist list: %v\n", err)
+		os.Exit(1)
 	}
 
 	listOutputStr := string(bytes)
@@ -29,23 +31,22 @@ func main() {
 			continue
 		}
 
-		os := segments[0]
-		arch := segments[1]
+		targetOs := segments[0]
+		targetArch := segments[1]
 
-		fileName := fmt.Sprintf("%s/%s_%s_%s", BUILD_DIR, BUILD_FILE, os, arch)
-		if strings.Contains(os, "windows") {
+		fileName := fmt.Sprintf("%s/%s_%s_%s", BUILD_DIR, BUILD_FILE, targetOs, targetArch)
+		if strings.Contains(targetOs, "windows") {
 			fileName += ".exe"
 		}
 
 		fmt.Printf("building %s... \n", fileName)
 
 		buildCmd := exec.Command("go", "build", "-o", fileName, SOURCE_DIR)
-		buildCmd.Env = append(buildCmd.Env, fmt.Sprintf("GOOS=%s", os), fmt.Sprintf("GOARCH=%s", arch))
+		buildCmd.Env = append(os.Environ(), fmt.Sprintf("GOOS=%s", targetOs), fmt.Sprintf("GOARCH=%s", targetArch))
 
-		err := buildCmd.Run()
-		if err != nil {
-			panic(err)
+		if out, err := buildCmd.CombinedOutput(); err != nil {
+			fmt.Fprintf(os.Stderr, "build %s/%s failed: %v\n%s", targetOs, targetArch, err, out)
+			os.Exit(1)
 		}
 	}
-
 }

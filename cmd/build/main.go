873d8dbe027b7f9ce9d8161375dbe98e1158230d
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-18 16:29:51 -0500

    parallel builds with wait group

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 const SOURCE_DIR = "./cmd/tui"
@@ -32,6 +33,7 @@ func main() {
 	listOutputStr := string(bytes)
 	listOutput := strings.Split(listOutputStr, "\n")
 
+	var wg sync.WaitGroup
 	for _, output := range listOutput {
 		segments := strings.Split(output, "/")
 		if len(segments) < 2 {
@@ -41,19 +43,34 @@ func main() {
 		targetOs := segments[0]
 		targetArch := segments[1]
 
-		fileName := fmt.Sprintf("%s/%s_%s_%s", BUILD_DIR, BUILD_FILE, targetOs, targetArch)
-		if strings.Contains(targetOs, "windows") {
-			fileName += ".exe"
-		}
-
-		fmt.Printf("building %s... \n", fileName)
-
-		buildCmd := exec.Command("go", "build", "-o", fileName, SOURCE_DIR)
-		buildCmd.Env = append(os.Environ(), fmt.Sprintf("GOOS=%s", targetOs), fmt.Sprintf("GOARCH=%s", targetArch))
-
-		if out, err := buildCmd.CombinedOutput(); err != nil {
-			fmt.Fprintf(os.Stderr, "build %s/%s failed: %v\n%s", targetOs, targetArch, err, out)
-			continue
-		}
+		wg.Add(1)
+		go build(targetOs, targetArch, &wg)
 	}
+
+	wg.Wait()
+	fmt.Println("build complete")
+}
+
+func build(targetOs, targetArch string, wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	fileName := fmt.Sprintf("%s/%s_%s_%s", BUILD_DIR, BUILD_FILE, targetOs, targetArch)
+	if strings.Contains(targetOs, "windows") {
+		fileName += ".exe"
+	}
+
+	fmt.Printf("building %s... \n", fileName)
+
+	buildCmd := exec.Command("go", "build", "-o", fileName, SOURCE_DIR)
+	buildCmd.Env = append(os.Environ(),
+		fmt.Sprintf("GOOS=%s", targetOs),
+		fmt.Sprintf("GOARCH=%s", targetArch))
+
+	if out, err := buildCmd.CombinedOutput(); err != nil {
+		fmt.Fprintf(os.Stderr, "build %s/%s failed: %v\n%s", targetOs, targetArch, err, out)
+		return
+	}
+
+	fmt.Printf("build %s... succeeded!\n", fileName)
 }

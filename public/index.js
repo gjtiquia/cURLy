7260560d78c886d06e6a37f4455cc8a2d4b559ab
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-19 00:27:40 -0500

    copy button feedback

--- public/index.js
+++ public/index.js
@@ -1,15 +1,28 @@
+// web/src/util.ts
+function sleepAsync(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 // web/src/copy-button.ts
 listenToCopyButton();
 function listenToCopyButton() {
   document.body.addEventListener("click", async (event) => {
-    const target = event.target;
-    if (!target.matches("[data-copy-button]"))
+    const button = event.target;
+    if (!button.matches("[data-copy-button]"))
       return;
-    const codeElement = target.parentElement?.querySelector("code");
+    const codeElement = button.parentElement?.querySelector("code");
     if (codeElement) {
+      if (button.innerHTML == "Copied!") {
+        console.log("[data-copy-button] debounced!");
+        return;
+      }
       const code = codeElement.innerHTML;
       await navigator.clipboard.writeText(code);
       console.log("[data-copy-button] copied ", code);
+      const originalInnerHTML = button.innerHTML;
+      button.innerHTML = "Copied!";
+      await sleepAsync(3000);
+      button.innerHTML = originalInnerHTML;
     }
   });
 }
--- web/src/copy-button.ts
+++ web/src/copy-button.ts
@@ -1,17 +1,31 @@
+import { sleepAsync } from "./util";
+
 listenToCopyButton()
 export function listenToCopyButton() {
     // subscribe to the submit event itself, instead of having each element subscribe to the submit event
     // this is because, hx-boost does not do full page reloads, so <script> tags will not be reloaded, and it wont subscribe to new elements
     document.body.addEventListener("click", async (event) => {
-        const target = event.target as HTMLElement;
-        if (!target.matches("[data-copy-button]")) return;
+        const button = event.target as HTMLElement;
+        if (!button.matches("[data-copy-button]")) return;
 
-        const codeElement = target.parentElement?.querySelector("code")
+        const codeElement = button.parentElement?.querySelector("code")
         if (codeElement) {
+            if (button.innerHTML == "Copied!") {
+                console.log("[data-copy-button] debounced!")
+                return;
+            }
+
             const code = codeElement.innerHTML
             await navigator.clipboard.writeText(code);
 
             console.log("[data-copy-button] copied ", code)
+
+            const originalInnerHTML = button.innerHTML
+            button.innerHTML = "Copied!"
+
+            await sleepAsync(3000)
+
+            button.innerHTML = originalInnerHTML
         }
     });
 }

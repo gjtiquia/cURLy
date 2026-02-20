19eff857a47278015b90d92a0e6ef2139a981e6b
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-20 01:19:48 -0500

    todos

--- web/src/wasm/index.ts
+++ web/src/wasm/index.ts
@@ -2,5 +2,5 @@ export { initAsync } from "./wasm";
 
 // TODO :
 // - Vector2 type, rename Addr to Ptr
-// - research code-gen to improve DX when adding export/import functions in go
+// - research code-gen to improve DX when adding export/import functions in go, and notify events
 // - extract game logic into a shared package that can be used by cmd/tui and cmd/wasm

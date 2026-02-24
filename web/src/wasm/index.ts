bf8986c8fe18c27e5076dfb75fc9fbb61f13fc7a
GJ Tiquia <gershom.tiquia@gmail.com>
2026-02-23 23:34:25 -0500

    show loading

--- web/src/wasm/index.ts
+++ web/src/wasm/index.ts
@@ -1,5 +1,1 @@
 export { initAsync } from "./wasm";
-
-// TODO :
-// - research code-gen to improve DX when adding export/import functions in go, and notify events
-// - extract game logic into a shared package that can be used by cmd/tui and cmd/wasm
